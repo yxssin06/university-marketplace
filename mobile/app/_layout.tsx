c75b215a5b6c0a619929c5e6ead12fbe4023cbac
Yassin Shaheen <yassinxs22@gmail.com>
2026-02-12 21:23:06 -0500

    login page

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,44 +1,5 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { Slot } from 'expo-router';
 
 export default function RootLayout() {
-  return (
-    <>
-      <StatusBar style="dark" />
-      <Stack screenOptions={{ headerShown: false }}>
-        {/* Auth Stack (Index is now Login) */}
-        <Stack.Screen 
-          name="index" 
-          options={{ 
-            headerShown: false,
-          }} 
-        />
-        <Stack.Screen 
-          name="university-select" 
-          options={{ 
-            presentation: 'modal',
-            headerShown: false,
-          }} 
-        />
-        
-        {/* App Stack */}
-        <Stack.Screen 
-          name="home" 
-          options={{ 
-            headerShown: true, 
-            title: 'Marketplace',
-            headerBackVisible: false 
-          }} 
-        />
-        <Stack.Screen 
-          name="item/[id]" 
-          options={{ 
-            title: 'Item Details',
-            headerBackTitle: 'Back',
-            headerShown: true
-          }} 
-        />
-      </Stack>
-    </>
-  );
+  return <Slot />;
 }
